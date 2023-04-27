f9889abc6ed5e20f50f71ceb1811db3896cc22ea
sphy <sphy.debugger@gmail.com>
2023-04-28 01:35:35 +0200

    Change test name

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,136 +1,134 @@
 import org.junit.Assert;
-import org.junit.Test;
 
 
+public class Test {
 
-public class TestKata {
-
-    @Test
+    @org.junit.Test
         public void pruebaUno(){
         KataMain kata = new KataMain();
         Assert.assertEquals("I", kata.convertirNumero(1));
     }
-    @Test
+    @org.junit.Test
     public void pruebaDos(){
         KataMain kata = new KataMain();
         Assert.assertEquals("II", kata.convertirNumero(2));
     }
-    @Test
+    @org.junit.Test
     public void pruebaTres(){
         KataMain kata = new KataMain();
         Assert.assertEquals("III", kata.convertirNumero(3));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCinco(){
         KataMain kata = new KataMain();
         Assert.assertEquals("V", kata.convertirNumero(5));
     }
-    @Test
+    @org.junit.Test
     public void pruebaSeis(){
         KataMain kata = new KataMain();
         Assert.assertEquals("VI", kata.convertirNumero(6));
     }
-    @Test
+    @org.junit.Test
     public void pruebaSiete(){
         KataMain kata = new KataMain();
         Assert.assertEquals("VII", kata.convertirNumero(7));
     }
-    @Test
+    @org.junit.Test
     public void pruebaOcho(){
         KataMain kata = new KataMain();
         Assert.assertEquals("VIII", kata.convertirNumero(8));
     }
-    @Test
+    @org.junit.Test
     public void pruebaDiez(){
         KataMain kata = new KataMain();
         Assert.assertEquals("X", kata.convertirNumero(10));
     }
-    @Test
+    @org.junit.Test
     public void pruebaTrece(){
         KataMain kata = new KataMain();
         Assert.assertEquals("XIII", kata.convertirNumero(13));
     }
-    @Test
+    @org.junit.Test
     public void pruebaQuince(){
         KataMain kata = new KataMain();
         Assert.assertEquals("XV", kata.convertirNumero(15));
     }
-    @Test
+    @org.junit.Test
     public void pruebaVeinte(){
         KataMain kata = new KataMain();
         Assert.assertEquals("XX", kata.convertirNumero(20));
     }
-    @Test
+    @org.junit.Test
     public void pruebaTreintaytres(){
         KataMain kata = new KataMain();
         Assert.assertEquals("XXXIII", kata.convertirNumero(33));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCuarentaysiete(){
         KataMain kata = new KataMain();
         Assert.assertEquals("XLVII", kata.convertirNumero(47));
     }
-    @Test
+    @org.junit.Test
     public void pruebaSesentaynueve(){
         KataMain kata = new KataMain();
         Assert.assertEquals("LXIX", kata.convertirNumero(69));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoTrece(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CXIII", kata.convertirNumero(113));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoQuince(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CXV", kata.convertirNumero(115));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoVeinte(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CXX", kata.convertirNumero(120));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoTreintaytres(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CXXXIII", kata.convertirNumero(133));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoCuarentaysiete(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CXLVII", kata.convertirNumero(147));
     }
-    @Test
+    @org.junit.Test
     public void pruebaCientoSesentaynueve(){
         KataMain kata = new KataMain();
         Assert.assertEquals("CLXIX", kata.convertirNumero(169));
     }
-    @Test
+    @org.junit.Test
     public void pruebaMilCientoTrece(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MCXIII", kata.convertirNumero(1113));
     }
-    @Test
+    @org.junit.Test
     public void pruebaMilCientoQuince(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MCXV", kata.convertirNumero(1115));
     }
-    @Test
+    @org.junit.Test
     public void pruebaMilCientoVeinte(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MCXX", kata.convertirNumero(1120));
     }
-    @Test
+    @org.junit.Test
     public void pruebaDosMilCientoTreintaytres(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MMCXXXIII", kata.convertirNumero(2133));
     }
-    @Test
+    @org.junit.Test
     public void pruebaDosMilCientoCuarentaysiete(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MMCXLVII", kata.convertirNumero(2147));
     }
-    @Test
+    @org.junit.Test
     public void pruebaDosMilCientoSesentaynueve(){
         KataMain kata = new KataMain();
         Assert.assertEquals("MMCLXIX", kata.convertirNumero(2169));
