19ee129d59b62c4e3a70c2d910f03efe818ed335
sphy <sphy.debugger@gmail.com>
2023-04-29 14:19:58 +0200

    Chage code to linter8

--- src/main/java/KataMain.java
+++ src/main/java/KataMain.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public final class KataMain {
 
 /** Método principal. */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         int numeroEntrada;
+        int MIN = 1;
+        int MAX = 3000;
+
 
         Scanner teclado = new Scanner(System.in);
 
@@ -13,43 +16,56 @@ public final class KataMain {
                 System.out.println("Introduce un número entre el 1 y el 3000 ");
                 System.out.println("Yo lo convertiré en números romanos.");
                 numeroEntrada = teclado.nextInt();
-            } while (numeroEntrada < 1 || numeroEntrada > 3000);
+            } while (numeroEntrada < MIN || numeroEntrada > MAX);
 
             String respuesta = convertirNumero(numeroEntrada);
-            System.out.println("El número " + numeroEntrada + " en romanos es " + respuesta);
+            System.out.println("El número "
+                 + numeroEntrada + " en romanos es " + respuesta);
     }
 
 /** Método para convertir a romano. */
     static String convertirNumero (int numero) {
+        int ENTRE10 = 10;
+        int ENTRE100 = 100;
+
+        int MIL = 1000;
+        int FINALMIL = 1999;
+        int DOSMIL = 2000;
+        int FINALDOSMIL = 2999;
+        int TRESMIL = 3000;
+
+        int CUATRO = 4;
+        int CINCO = 5;
+        int SEIS = 6;
+        int NUEVE = 9;
+
+
         String numeroEnRomano = "";
         final String unoRomano ="I";
-        final int unidades;
-        final int decenas;
-        final int centenas;
+        final int unidades = numero % ENTRE10;
+        final int decenas = numero / ENTRE10 % ENTRE10;
+        final int centenas = numero / ENTRE100 % ENTRE10;
 
-        decenas = numero / 10 % 10;
-        unidades = numero % 10;
-        centenas = numero / 100 % 10;
 
-        if (numero >= 1000 && numero<=1999) {
+        if (numero >= MIL && numero<=FINALMIL) {
             numeroEnRomano = numeroEnRomano + "M";
         }
-        if (numero >= 2000 && numero<=2999) {
+        if (numero >= DOSMIL && numero<=FINALDOSMIL) {
             numeroEnRomano = numeroEnRomano + "MM";
         }
-        if (numero == 3000) {
+        if (numero == TRESMIL) {
             numeroEnRomano = numeroEnRomano + "MMM";
         }
 
 
-        if (centenas == 9) {
+        if (centenas == NUEVE) {
             numeroEnRomano = numeroEnRomano + "CM";
-        } else if (centenas >= 5) {
+        } else if (centenas >= CINCO) {
             numeroEnRomano = numeroEnRomano + "D";
-            for (int i = 6; i <= centenas; i++) {
+            for (int i = SEIS; i <= centenas; i++) {
                 numeroEnRomano = numeroEnRomano + "C";
             }
-        } else if (centenas == 4) {
+        } else if (centenas == CUATRO) {
             numeroEnRomano = numeroEnRomano + "CD";
         } else {
             for (int i = 1; i <= centenas; i++) {
@@ -57,14 +73,14 @@ public final class KataMain {
             }
         }
 
-        if (decenas == 9) {
+        if (decenas == NUEVE) {
             numeroEnRomano = numeroEnRomano + "XC";
-        } else if (decenas >= 5) {
+        } else if (decenas >= CINCO) {
             numeroEnRomano = numeroEnRomano + "L";
-            for (int i = 6; i <= decenas; i++) {
+            for (int i = SEIS; i <= decenas; i++) {
                 numeroEnRomano = numeroEnRomano + "X";
             }
-        } else if (decenas == 4) {
+        } else if (decenas == CUATRO) {
             numeroEnRomano = numeroEnRomano + "XL";
         } else {
             for (int i = 1; i <= decenas; i++) {
@@ -72,14 +88,14 @@ public final class KataMain {
             }
         }
 
-        if (unidades == 9) {
+        if (unidades == NUEVE) {
             numeroEnRomano = numeroEnRomano + "IX";
-        } else if (unidades >= 5) {
+        } else if (unidades >= CINCO) {
             numeroEnRomano = numeroEnRomano + "V";
-            for (int i = 6; i <= unidades; i++) {
+            for (int i = SEIS; i <= unidades; i++) {
                 numeroEnRomano = numeroEnRomano + unoRomano;
             }
-        } else if (unidades == 4) {
+        } else if (unidades == CUATRO) {
             numeroEnRomano = numeroEnRomano + "IV";
         } else {
             for (int i = 1; i <= unidades; i++) {
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,182 +7,182 @@ public class Test {
     /** Probando el número 1. */
     @org.junit.Test
         public void pruebaUno() {
-        int numeroPrueba = 1;
+        final int numeroPrueba = 1;
         Assert.assertEquals("I", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 2. */
     @org.junit.Test
     public void pruebaDos() {
-        int numeroPrueba = 2;
+        final int numeroPrueba = 2;
         Assert.assertEquals ("II", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 3. */
     @org.junit.Test
     public void pruebaTres() {
-        int numeroPrueba = 3;
+        final int numeroPrueba = 3;
         Assert.assertEquals("III", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 5. */
     @org.junit.Test
     public void pruebaCinco() {
-        int numeroPrueba = 5;
+        final int numeroPrueba = 5;
         Assert.assertEquals("V", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 6. */
     @org.junit.Test
     public void pruebaSeis() {
-        int numeroPrueba = 6;
+        final int numeroPrueba = 6;
         Assert.assertEquals("VI", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 7. */
     @org.junit.Test
     public void pruebaSiete() {
-        int numeroPrueba = 7;
+        final int numeroPrueba = 7;
         Assert.assertEquals("VII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 8. */
     @org.junit.Test
     public void pruebaOcho() {
-        int numeroPrueba = 8;
+        final int numeroPrueba = 8;
         Assert.assertEquals("VIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 10. */
     @org.junit.Test
     public void pruebaDiez() {
-        int numeroPrueba = 10;
+        final int numeroPrueba = 10;
         Assert.assertEquals("X", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 13. */
     @org.junit.Test
     public void pruebaTrece() {
-        int numeroPrueba = 13;
+        final int numeroPrueba = 13;
         Assert.assertEquals("XIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 15. */
     @org.junit.Test
     public void pruebaQuince() {
-        int numeroPrueba = 15;
+        final int numeroPrueba = 15;
         Assert.assertEquals("XV", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 20. */
     @org.junit.Test
     public void pruebaVeinte() {
-        int numeroPrueba = 20;
+        final int numeroPrueba = 20;
         Assert.assertEquals("XX", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 33. */
     @org.junit.Test
     public void pruebaTreintaytres() {
-        int numeroPrueba = 33;
+        final int numeroPrueba = 33;
         Assert.assertEquals("XXXIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 47. */
     @org.junit.Test
     public void pruebaCuarentaysiete() {
-        int numeroPrueba = 47;
+        final int numeroPrueba = 47;
         Assert.assertEquals("XLVII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 69. */
     @org.junit.Test
     public void pruebaSesentaynueve() {
-        int numeroPrueba = 69;
+        final int numeroPrueba = 69;
         Assert.assertEquals("LXIX", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 113. */
     @org.junit.Test
     public void pruebaCientoTrece() {
-        int numeroPrueba = 113;
+        final int numeroPrueba = 113;
         Assert.assertEquals("CXIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 115. */
     @org.junit.Test
     public void pruebaCientoQuince() {
-        int numeroPrueba = 115;
+        final int numeroPrueba = 115;
         Assert.assertEquals("CXV", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 120. */
     @org.junit.Test
     public void pruebaCientoVeinte() {
-        int numeroPrueba = 120;
+        final int numeroPrueba = 120;
         Assert.assertEquals("CXX", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 133. */
     @org.junit.Test
     public void pruebaCientoTreintaytres() {
-        int numeroPrueba = 133;
+        final int numeroPrueba = 133;
         Assert.assertEquals("CXXXIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 147. */
     @org.junit.Test
     public void pruebaCientoCuarentaysiete() {
-        int numeroPrueba = 147;
+        final int numeroPrueba = 147;
         Assert.assertEquals("CXLVII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 169. */
     @org.junit.Test
     public void pruebaCientoSesentaynueve() {
-        int numeroPrueba = 169;
+        final int numeroPrueba = 169;
         Assert.assertEquals("CLXIX", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 1113. */
     @org.junit.Test
     public void pruebaMilCientoTrece() {
-        int numeroPrueba = 1113;
+        final int numeroPrueba = 1113;
         Assert.assertEquals("MCXIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 1115. */
     @org.junit.Test
     public void pruebaMilCientoQuince() {
-        int numeroPrueba = 1115;
+        final int numeroPrueba = 1115;
         Assert.assertEquals("MCXV", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 1120. */
     @org.junit.Test
     public void pruebaMilCientoVeinte() {
-        int numeroPrueba = 1120;
+        final int numeroPrueba = 1120;
         Assert.assertEquals("MCXX", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 2133. */
     @org.junit.Test
     public void pruebaDosMilCientoTreintaytres() {
-        int numeroPrueba = 2133;
+        final int numeroPrueba = 2133;
         Assert.assertEquals("MMCXXXIII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 2147. */
     @org.junit.Test
     public void pruebaDosMilCientoCuarentaysiete() {
-        int numeroPrueba = 2147;
+        final int numeroPrueba = 2147;
         Assert.assertEquals("MMCXLVII", KataMain.convertirNumero(numeroPrueba));
     }
 
     /** Probando el número 2169. */
     @org.junit.Test
     public void pruebaDosMilCientoSesentaynueve() {
-        int numeroPrueba = 2169;
+        final int numeroPrueba = 2169;
         Assert.assertEquals("MMCLXIX", KataMain.convertirNumero(numeroPrueba));
     }
 
